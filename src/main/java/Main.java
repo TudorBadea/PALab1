77af8daa0d4ce6df81f9864ed2cd37eb4aab1c1f
Tudor Badea <tudor.badea99@gmail.com>
2021-02-19 13:32:03 +0200

    problema obligatorie

---
+++ src/main/java/Main.java
@@ -0,0 +1,31 @@
+import java.lang.Math;
+
+public class Main<n> {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        String[] languages = {"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
+
+        int n = (int) (Math.random() * 1_000_000);
+        n = n * 3;
+        byte binary = 0b10101;
+        n = n + binary;
+        int hex = 0xFF;
+        n = n + hex;
+        n = n * 6;
+        int sum = 0;
+        while (n > 9) {
+            sum = 0;
+            while (n > 0) {
+                sum += n % 10;
+                n /= 10;
+            }
+            n = sum;
+        }
+        System.out.println(n);
+        System.out.println("Willy-nilly, this semester I will learn " + languages[n]);
+    }
+
+
+}
